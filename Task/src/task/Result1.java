64f99d5452497599bf106cb70b453b63a3cfbf4e
Real Dewan <dewanreal@gmail.com>
2021-11-16 02:44:03 +0600

    Final Task is finally done

--- Task/src/task/Result1.java
+++ Task/src/task/Result1.java
@@ -3,12 +3,10 @@ package task;
 public class Result1 {
     float add(float a, float b) {
         float s1 = a + b;
-        System.out.println(s1); //for resolving only
         return s1;
     }
     float sub(float b, float c) {
         float s2 = b - c;
-        System.out.println("sub " + s2); //for resolving only
         return s2;
         
     }
--- Task/src/task/Task.java
+++ Task/src/task/Task.java
@@ -19,15 +19,21 @@ public class Task {
         float c = sc.nextInt();
         
         Result1 obj = new Result1();
-        float add = obj.add(a, b);
+        float add = obj.add(a, b); // a + b
         
-        float sub = obj.sub(b, c);
+        float sub = obj.sub(b, c); // b - c
         /* Mult & Div */
         Result2 objs = new Result2();
-        float mult = objs.mult(a, b);
+        float mult = objs.mult(a, b); // a * b
         
-        objs.div1(add, 2);
-        objs.div2(sub, a);
+        float a3 = objs.div1(add, 2); // (a + b) / 2
+        float b3 =objs.div2(sub, a);
+        
+        //Final
+        float c3 = mult;
+        float finalAns = objs.finalS(a3, b3, c3);
+        System.out.println("The Final Answer is: " + finalAns);
+        
         
     }
     
